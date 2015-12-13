66c43397046dcd1170884233a0f366fbd907ca53
Alex <alexonej@gmail.com>
2015-12-13 00:07:09 -0800

    added bubblesort

--- src/SortingAlgorithms/BubbleSort.java
+++ src/SortingAlgorithms/BubbleSort.java
@@ -5,15 +5,52 @@ package SortingAlgorithms;
  */
 public class BubbleSort {
 
-    int[] toSort = {1,6,7,9,3,5,6,8,234,3,8};
 
-    public int[] bubbleSort(int[] toSort) {
+    public static void bubbleSort(int[] toSort) {
+
+        int temp;
+
+        for (int i = 0; i < toSort.length; i++) {
+
+            for (int j = 1; j < (toSort.length - i); j++) {
 
 
-
-
-        return null; 
+                if (toSort[j-1] > toSort[j]) {
+                    temp = toSort[j-1];
+                    toSort[j-1] = toSort[j];
+                    toSort[j] = temp;
+                }
+            }
+        }
     }
+
+
+
+    public static void main(String [ ] args)
+    {
+
+        int[] toSort = {1,6,7,9,21323,5,6,12,234,3,8};
+
+        System.out.println("Array Before Bubble Sort");
+        for(int i=0; i < toSort.length; i++){
+            System.out.print(toSort[i] + " ");
+        }
+
+        bubbleSort(toSort);
+
+        System.out.println("");
+
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < toSort.length; i++){
+            System.out.print(toSort[i] + " ");
+        }
+
+
+
+
+    }
+
+
 
 
 
